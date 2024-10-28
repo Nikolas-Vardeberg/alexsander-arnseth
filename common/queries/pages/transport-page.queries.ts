587ebeab3c1f5a8b8217b83f0836974db6c5530c
Nikolas, Vardeberg - Rek <156543836+Nikolas-Vardeberg@users.noreply.github.com>
2024-10-28 14:39:04 +0100

    gettransportpage query

---
+++ common/queries/pages/transport-page.queries.ts
@@ -0,0 +1,15 @@
+import { groq } from "next-sanity";
+import { BLOCKS_QUERY } from "../blocks/index.queries";
+import { seo } from "./seo.queries";
+
+export const getTransportPageQuery = groq`*[_type == "transportPage" && slug.current == $slug][0]{
+    _id,
+    title,
+    mainImage -> {
+        "url": asset->url,
+    },
+    "slug": slug.current,
+    excerpt,
+    "blocks": blocks[]${BLOCKS_QUERY},
+    ${seo},
+}`;
