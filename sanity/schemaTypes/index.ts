a23a8309329d44028f45625d8b29837745f740e9
Nikolas, Vardeberg - Rek <156543836+Nikolas-Vardeberg@users.noreply.github.com>
2024-10-28 09:27:20 +0100

    creating article schema with seo

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { article } from './pages/article'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [article],
 }
---
+++ sanity/schemaTypes/pages/article.ts
@@ -0,0 +1,90 @@
+import { defineType } from "sanity";
+import { seo } from "../other/seo";
+import { Newspaper } from "lucide-react";
+
+export const article = defineType({
+    type: "document",
+    name: "article",
+    title: "Article",
+    icon: Newspaper,
+    groups: [
+        {
+            name: "general",
+            title: "General",
+            default: true,
+        },
+        {
+            name: "byline",
+            title: "Byline",
+        },
+        {
+            name: "seo",
+            title: "SEO",
+        },
+    ],
+    fields: [
+        {
+            name: "title",
+            type: "string",
+            title: "Title",
+            group: "general",
+        },
+        {
+            name: "mainImage",
+            type: "image",
+            title: "Main image",
+            description: "The main image of the article",
+            options: {
+                hotspot: true,
+            },
+            validation: (Rule) => Rule.required(),
+            group: "general",
+        },
+        {
+            name: "slug",
+            type: "slug",
+            options: {
+                source: 'title',
+            },
+            group: "general",
+        },
+        //TODO: TAGS
+        {
+            name: "publishedAt",
+            type: "datetime",
+            title: "Pulisert",
+            description: "Dato for publisering av artikkelen",
+            group: "byline",
+            initialValue: new Date().toISOString(),
+            validation: (Rule) => Rule.required(),
+        },  
+        {
+            ...seo,
+            group: "seo",
+        },
+        //TODO:BLOCKS
+        //TODO: EDITOR
+    ],
+    orderings: [
+        {
+            title: "Publiseringsdato (nyeste først)",
+            name: "publishDateDesc",
+            by: [{ field: "publishedAt", direction: 'desc'}],
+        },
+        {
+            title: "Publiseringsdato (eldste først)",
+            name: "publishDateAsc",
+            by: [{ field: "publishedAt", direction: "asc"}],
+        },
+        {
+            title: "Tittel (A-Å)",
+            name: "titleAsc",
+            by: [{ field: "title", direction: "asc"}],
+        },
+        {
+            title: "Tittel (A-Å)",
+            name: "titleDesc",
+            by: [{ field: 'title', direction: "asc"}],
+        }
+    ]
+})
