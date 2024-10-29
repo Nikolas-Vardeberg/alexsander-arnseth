25c1f272da163361e0d53f6c960bb639133cc602
niklusrook <nikolas.vardeberg1@gmail.com>
2024-10-29 15:07:44 +0100

    feat: custom font space-grotesk

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,12 @@ import { DisableDraftMode } from "@/common/components/sanity/DisableDraftMode";
 import { VisualEditing } from "next-sanity";
 import { SanityLive } from "@/sanity/lib/live";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGrotesk.woff",
+  variable: "--font-space-grotesk",
   weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -32,7 +28,7 @@ export default async function RootLayout({
     <ReactQueryProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${spaceGrotesk.className} antialiased`}
         >
           {children}
           <SanityLive />
